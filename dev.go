3370f6ada6dacfb92b4d7d05e5962bc07cf473fc
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2022-11-12 20:02:16 +0800

    Create dev.go

---
+++ dev.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func Nextln(){
+
+Println("\n")
+
+
+}
+
+
+func main(){
+
+fmt.Println("Dogs == cant fly")
+Nextln()
+fmt.Println("Fish == cant walk")
+Nextln()
+fmt.Println("Birds == cant swim")
+Nextln()
+fmt.Println("Ducks == smiles like a FullstackDeveloper")
+
+}
+
+//its an analogy of what a fullstack developer is if you dont get it.
