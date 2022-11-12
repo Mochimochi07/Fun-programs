37af20154bb5d1c7ab6b31bbb72bf87f0188cfbd
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2022-11-12 20:13:54 +0800

    Update dormammu.go

--- dormammu.go
+++ dormammu.go
@@ -9,9 +9,10 @@ func main() {
 	
 	for true {
 		fmt.Println("Stephen Strange: Dormammu I've come to bargain!")
+time.Sleep(time.Second)
 		fmt.Println("*Stephen dies \n")
+time.Sleep(time.Second)
 fmt.Println("*Gets reborn")
-
 time.Sleep(time.Second)
 	}
 }
