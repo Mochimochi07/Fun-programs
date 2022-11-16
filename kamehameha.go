dfa66d4453736e7830f533e783b347f9b67f6a1c
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2022-11-16 20:51:49 +0800

    Create kamehameha.go

---
+++ kamehameha.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+
+
+/* just a regular bootleg anime while piccolo was just being a wholesome dad to gohan*/
+
+
+
+
+
+
+
+func main() {
+
+	var a int
+
+	fmt.Println("A new episode to dragon ball Z! \n")
+	time.Sleep(time.Second)
+	fmt.Println("*show title\n")
+	time.Sleep(time.Second)
+	fmt.Println("Title: Father son kamehameha!\n")
+	time.Sleep(time.Second)
+	fmt.Println("*title fades and proceeds to episode\n")
+	time.Sleep(time.Second)
+	for a = 0; a <= 5; a++ {
+
+		switch a {
+
+		case 0:
+
+			fmt.Println("*shows too much flash like porygon's episode")
+      time.Sleep(time.Second)
+			fmt.Println("Gohan: KAAAAMMMEEEE")
+			time.Sleep(time.Second)
+		case 1:
+
+			fmt.Println("Gohan: HAAAAAAMEEEEEE")
+			time.Sleep(time.Second)
+
+		case 2:
+
+			fmt.Println("* the channel is being interrupted by 3 minute ads.")
+			time.Sleep(time.Second)
+			fmt.Println("*showing dove ad.")
+			time.Sleep(time.Second)
+			fmt.Println("*ad ends.")
+			time.Sleep(time.Second)
+			fmt.Println("*showing chookstogo ad.")
+			time.Sleep(time.Second)
+			fmt.Println("*ad ends.")
+			time.Sleep(time.Second)
+			fmt.Println("*showing yakisoba ad.")
+			time.Sleep(time.Second)
+			fmt.Println("*ad ends.")
+			time.Sleep(time.Second)
+
+		case 3:
+
+			fmt.Println("*redirects back to the show")
+			time.Sleep(time.Second)
+			fmt.Println("Goku's spirit appears")
+			time.Sleep(time.Second)
+			fmt.Println("Gohan: DAD! NOW!")
+			time.Sleep(time.Second)
+			fmt.Println("Piccolo appears")
+			time.Sleep(time.Second)
+			fmt.Println("Piccolo: Alright!")
+			time.Sleep(time.Second)
+			fmt.Println("Gohan: Alright!")
+			time.Sleep(time.Second)
+
+		case 4:
+
+			fmt.Println("Gohan: HAAAAAAAA!")
+			time.Sleep(time.Second)
+			fmt.Println("Goku: HAAAAAAAA!")
+			time.Sleep(time.Second)
+			fmt.Println("Piccolo: Special Beam cannon!")
+      
+      time.Sleep(time.Second)
+			fmt.Println("*Cell obliterated ")
+      
+      
+		}
+
+	}
+
+  
+  
+  
+  
+}
