f3b80c3e22db595fbcf90817982425552e3d65fd
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2022-11-16 20:13:40 +0800

    Create kamehameha.go

---
+++ You can edit this code!/kamehameha.go
@@ -0,0 +1,32 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hey")
+
+	var a int
+
+	for a = 0; a <= 3; a++ {
+
+		fmt.Println(a)
+
+		switch a {
+
+		case 0:
+
+		case 1:
+
+			
+
+		case 2:
+
+			
+
+		}
+
+	}
+
+}
